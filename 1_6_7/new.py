e2cdf7eed7585aea076469b238761e120d914901
StealthyFox <kopulkina@gmail.com>
2016-05-01 06:34:17 +0300

    Update new.py

--- 1_6_7/new.py
+++ 1_6_7/new.py
@@ -3,16 +3,14 @@ import os
 
 s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)                          
 s.bind(('0.0.0.0', 2222))                                                       
-pid  = os.fork()
 s.listen(10)
-while True                                                        
-        conn, addr = s.accept()                                                 
-        while True:                                                             
-                data = conn.recv(1024)                                          
-                if not data:                                                    
-                        break                                                   
-                conn.send(data)                                                 
-                if data == 'close':
-                        break                                       
-        conn.close()                                                            
- 
+pid  = os.fork()
+while pid == 0:
+        while True                                                        
+                conn, addr = s.accept()                                                 
+                while True:                                                             
+                        data = conn.recv(1024)                                          
+                        if not data: break                                                   
+                        conn.send(data)                                                 
+                        if data == 'close': break                                       
+                conn.close()                                                            
