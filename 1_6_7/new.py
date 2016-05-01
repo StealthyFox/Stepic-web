03ca6f2da9cbc86444bdeaf8da372c674aec0308
StealthyFox <kopulkina@gmail.com>
2016-05-01 15:13:45 +0300

    Update new.py

--- 1_6_7/new.py
+++ 1_6_7/new.py
@@ -4,19 +4,16 @@ import os
 s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)                          
 s.bind(('0.0.0.0', 2222))                                                       
 s.listen(10)
-for i in range(10):                                                             
-        pid  = os.fork()                                                        
-        if pid == 0:                                                            
-                print('---pid', pid)                                            
-                while True:                                                     
+while True:                                                            
+        pid  = os.fork() 
+        if pid == 0:
+                conn, addr = s.accept()                                 
+                print ('neeew', addr)                                   
+                data = conn.recv(1024)                                  
                                                                                 
-                        conn, addr = s.accept()                                 
-                        print ('neeew', addr)                                   
-                        data = conn.recv(1024)                                  
+                if not data: break                                      
+                print('data is', data)                                  
+                conn.send(data)                                         
                                                                                 
-                        if not data: break                                      
-                        print('data is', data)                                  
-                        conn.send(data)                                         
-                                                                                
-                print('close it')                                               
-                conn.close()
+        print('close it')                                               
+        conn.close()
