7ef1e08157112b20b62b88af46a4a9e96d723c90
StealthyFox <kopulkina@gmail.com>
2016-05-01 17:14:13 +0300

    Update new.py

--- 1_6_7/new.py
+++ 1_6_7/new.py
@@ -1,19 +1,24 @@
+'''
+Разработать простейший TCP echo сервер.
+
+Требования
+
+Запускается на IP адресе 0.0.0.0 и TCP порту 2222 
+Получает сообщения длинной не более 1024 байт и отправляет обратно клиенту
+Закрывает соединение при получении сообщения с текстом close﻿
+'''
 import socket                                                                   
 import os
 
 s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)                          
 s.bind(('0.0.0.0', 2222))                                                       
-s.listen(10)
+s.listen(1)
 while True:                                                            
-        pid  = os.fork() 
-        if pid == 0:
-                conn, addr = s.accept()                                 
-                print ('neeew', addr)                                   
+        conn, addr = s.accept()                                 
+        while True:                                 
                 data = conn.recv(1024)                                  
-                                                                                
                 if not data: break                                      
-                print('data is', data)                                  
-                conn.send(data)                                         
-                                                                                
+                conn.send(data)  
+                if data == 'close': break
         print('close it')                                               
         conn.close()
