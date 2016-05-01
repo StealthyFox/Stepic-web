c672694634ca4f07acb6d9d454223b37dae49940
StealthyFox <kopulkina@gmail.com>
2016-05-01 15:25:50 +0300

    Update n.py

--- 1_6_7/n.py
+++ 1_6_7/n.py
@@ -5,13 +5,15 @@ s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))                                                       
 s.listen(10)
 while True:                                                            
-                conn, addr = s.accept()                                 
-                print ('neeew', addr)                                   
+        conn, addr = s.accept()                                 
+        print ('neeew', addr)
+        pid  = os.fork() 
+        if pid == 0:
                 data = conn.recv(1024)                                  
-                                                                                
-                if not data: break                                      
-                print('data is', data)                                  
-                conn.send(data)                                         
-                                                                                
-        print('close it')                                               
-        conn.close()
+                whilel True:
+                        if not data: break                                      
+                        print('data is', data)                                  
+                        conn.send(data)
+                        if data == 'close': break
+                print('close it')
+                conn.close()
