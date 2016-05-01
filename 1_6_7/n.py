c594db84e19551accb5be1fa45e9103d3eefc7b9
StealthyFox <kopulkina@gmail.com>
2016-05-01 16:10:54 +0300

    Update n.py

--- 1_6_7/n.py
+++ 1_6_7/n.py
@@ -6,14 +6,12 @@ s.bind(('0.0.0.0', 2222))
 s.listen(10)
 while True:                                                            
         conn, addr = s.accept()                                 
-        print ('neeew', addr)
-        pid  = os.fork() 
-        if pid == 0:
-                data = conn.recv(1024)                                  
-                whilel True:
-                        if not data: break                                      
-                        print('data is', data)                                  
-                        conn.send(data)
-                        if data == 'close': break
-                print('close it')
+        print ('neeew', addr) 
+        pid = os.fork()
+        if pid ==0:
+                data = conn.recv(1024)
+                if not data: break                                      
+                print('data is', data)                                  
+                conn.send(data)                                         
+                print('close it')                                               
                 conn.close()
